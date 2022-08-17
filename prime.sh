654071f3f16846ef4ce6a114d4f2442f502db9ae
rajugajjela <rgajjela@innominds.com>
2022-08-17 10:58:34 +0530

    checking prime number or not

---
+++ prime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read num
+count=0
+for((i=1;i<=num;i++))
+do
+  a=`expr $num % $i`
+  if [ $a -eq 0 ]
+  then
+    count=`expr $count + 1`
+   fi
+done
+if [ $count -eq 2 ]
+   then
+    echo "prime"
+    else
+    echo "not prime"
+    fi
+
+
